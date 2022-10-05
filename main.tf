ff7b47d5451d1b63572dadeb12588056b763ee26
Matt Prentice <114755386+matt-prentice1@users.noreply.github.com>
2022-10-05 08:37:59 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,35 @@
+variable "gcp_cred" {
+  type = string
+  sensitive = true
+  description = "Google Cloud service account credentials"
+}
+
+provider "google" {
+    project = "a0df6031b"
+    credentials = var.gcp_cred
+    region="us-central1"
+    zone="us-central1-c"
+}
+
+resource "google_compute_instance" "pxe-server" {
+  name         = "pxe-server"
+  machine_type = "e2-medium"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1804-bionic-v20220901"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "pxe-server"
+  auto_create_subnetworks = "true"
+}
